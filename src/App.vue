20ac4e624a22e423c915142bc8b18b2c3e0f06ec
Bewizi <toluwalase.bewaji@yahoo.com>
2025-05-05 16:59:28 +0100

    Add initial App.vue component with layout and routing setup

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import AppHeader from './components/AppHeader.vue'
+</script>
+
+<template>
+  <main
+    class="bg-[url('/images/background.png')] h-[150vh] sm:h-screen md:h-screen lg:h-screen bg-cover bg-no-repeat lg:overflow-hidden"
+  >
+    <AppHeader />
+    <RouterView />
+  </main>
+</template>
+
+<style scoped></style>

614a98803b85dd2296263b85934ef6c1f43bbac5
Bewizi <toluwalase.bewaji@yahoo.com>
2025-05-05 17:00:06 +0100

    Add AppLink component for reusable navigation links

---
+++ src/components/AppLink.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+
+defineProps<{
+  to: string
+  className?: string
+}>()
+</script>
+
+<template>
+  <RouterLink :to="to" :class="className">
+    <slot />
+  </RouterLink>
+</template>
+
+<style scoped></style>

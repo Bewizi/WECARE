e1e31c5cbe2661d83e5d862625d1c8ffaa94251d
Bewizi <toluwalase.bewaji@yahoo.com>
2025-05-05 16:59:56 +0100

    Add AppHeader component with layout and navigation links

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import AppContainer from './AppContainer.vue'
+import AppLink from './AppLink.vue'
+</script>
+<template>
+  <AppContainer class="lg:pt-14 hidden lg:block">
+    <div
+      class="border-2 border-black px-8 py-2 rounded-lg shadow-[-1px_1.56px_1px_1px_rgba(0,0,0,0.9)] shadow-black bg-white"
+    >
+      <header class="flex items-center justify-between">
+        <div>
+          <AppLink to="/" class="font-kanit font-bold text-2xl">WeCare</AppLink>
+        </div>
+
+        <nav>
+          <ul class="flex justify-between items-center space-x-60">
+            <li>
+              <AppLink to="/" class="font-kanit text-xl">Whitepaper</AppLink>
+            </li>
+            <li>
+              <AppLink to="/" class="font-kanit text-xl">Telegram</AppLink>
+            </li>
+          </ul>
+        </nav>
+
+        <div
+          class="bg-[#A1FB05] px-8 py-2 rounded-xl border-2 border-black shadow-[-1px_1.56px_1px_1px_rgba(0,0,0,0.9)] shadow-black"
+        >
+          <button class="font-bold font-kanit text-lg">Chart</button>
+        </div>
+      </header>
+    </div>
+  </AppContainer>
+</template>
+
+<style scoped></style>

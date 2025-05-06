6cda4a195f1ddc8aefe6881e999d3ed44fb9ef3a
Bewizi <toluwalase.bewaji@yahoo.com>
2025-05-06 08:24:44 +0100

    Add routes for Whitepaper and Telegram views in the router

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,6 +17,16 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/whitepaper',
+      name: 'whitepaper',
+      component: () => import('@/views/WhitepaperView.vue'),
+    },
+    {
+      path: '/telegram',
+      name: 'telegram',
+      component: () => import('@/views/TelegramView.vue'),
+    },
   ],
 })
 

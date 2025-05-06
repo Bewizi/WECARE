8e0c424f6b3dbb38d719ccde42b53f05fd192448
Bewizi <toluwalase.bewaji@yahoo.com>
2025-05-06 11:14:31 +0100

    Add intersection observer to animate main background on scroll

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,31 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import AppHeader from './components/AppHeader.vue'
+import { useIntersectionObserver } from '@vueuse/core'
+import { onMounted, ref } from 'vue'
+
+const imageRef = ref<HTMLElement | null>(null)
+
+onMounted(() => {
+  useIntersectionObserver(
+    imageRef,
+    ([entry]) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('animate__animated', 'animate__fadeIn')
+      } else {
+        entry.target.classList.remove('animate__animated', 'animate__fadeIn')
+      }
+    },
+    {
+      threshold: 0.5,
+    },
+  )
+})
 </script>
 
 <template>
   <main
+    ref="imageRef"
     class="bg-[url('/images/background.png')] h-[150vh] sm:h-screen md:h-screen lg:h-screen bg-cover bg-no-repeat lg:overflow-hidden"
   >
     <AppHeader />
